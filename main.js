5a619910f92981170a0f081d1251cfb105a6e1d6
Piotr <piotr.barnas13@gmail.com>
2019-04-06 00:09:28 +0200

    Ikony pogody

--- main.js
+++ main.js
@@ -1,41 +1,54 @@
-//var i=Math.floor(Math.random() * 12);
-//var weathersList = ['sunrise', 'cloudySunset', 'sunset', 'windySunset', 'rain', 'snowfall', 'thunderstorm', 'cloudy', 'cloudyNight', 'cloudy & windy', 'night', 'windy & sunny'];
-var weather = 'cloudy';
+var i=Math.floor(Math.random() * 12);
+var weathersList = ['sunrise', 'sunset', 'rain', 'snowfall', 'thunderstorm', 'cloudy', 'cloudy & windy', 'night', 'wind'];
+//var weather = weathersList[i];
+var weather = 'wind';
 var backgroundVideo = document.getElementById('bg_src');
+var icon = document.getElementsByClassName('icon');
+console.log(icon[0])
 if (weather== 'sunrise') {
     backgroundVideo.setAttribute('src', 'video/sunrise.mp4');
+    icon[0].src='img/sunrise.svg';
 } else if (weather == 'sunset') {
     backgroundVideo.setAttribute('src', 'video/sunset.mp4');
-} else if (weather == 'cloudySunset') {
-    backgroundVideo.setAttribute('src', 'video/cloudySunset.mp4');
-} else if (weather == 'windySunset') {
-    backgroundVideo.setAttribute('src', 'video/windySunset.mp4');
-} else if (weather == 'rain') {
+    icon[0].src='img/sunset.svg';
+}else if (weather == 'rain') {
     backgroundVideo.setAttribute('src', 'video/rain.mp4');
+    //icon[0].src='img/rain.svg';
 } else if (weather == 'snowfall') {
     backgroundVideo.setAttribute('src', 'video/snowfall.mp4');
+    icon[0].src='img/snowflake.svg';
 } else if (weather == 'thunderstorm') {
     backgroundVideo.setAttribute('src', 'video/thunderstorm.mp4');
+    icon[0].src='img/bolt.svg';
 } else if (weather == 'cloudy') {
     backgroundVideo.setAttribute('src', 'video/cloudy.mp4');
+    icon[0].src='img/clouds(1).svg';
 } else if (weather == 'cloudyNight') {
     backgroundVideo.setAttribute('src', 'video/cloudyNight.mp4');
-} else if (weather == 'cloudy & windy') {
-    backgroundVideo.setAttribute('src', 'video/cloudy & windy.mp4');
 } else if (weather == 'night') {
     backgroundVideo.setAttribute('src', 'video/night.mp4');
-} else if (weather == 'windy & sunny') {
-    backgroundVideo.setAttribute('src', 'video/windy & sunny.mp4');
+    icon[0].src='img/moon.svg';
+} else if (weather == 'wind') {
+    backgroundVideo.setAttribute('src', 'https: //www.pexels.com/video/video-of-wind-turbine-855792/');
+    //icon[0].src = 'img/moon.svg';
 }
 
+
+//console.log(x);
+//var listEl = document.createElement('Li');
+//listEl.appendChild(document.createTextNode())
+//document.getElementById("pasekMiast").appendChild(listEl)
+//console.log(listEl)
+
+
+
 function filtrowanie() {
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, ul, li, i, txtValue;
     input = document.getElementById('wyszukiwarka');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("pasekMiast");
     li = ul.getElementsByTagName('li');
-    console.log(li[i]);
-    for (i = 0; i < li.length; i++) {
+    ul = document.getElementById("pasekMiast");
+    for (i = 0; i < data.length; i++) {
         txtValue = li[i].innerText;
         if (txtValue.toUpperCase().indexOf(filter) == 0 && filter != '') {
             li[i].style.display = "initial";
