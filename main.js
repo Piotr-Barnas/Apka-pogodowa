a2a0e1ccfe37f91954f2872aed5821cf1c659571
Piotr <piotr.barnas13@gmail.com>
2019-04-06 10:41:19 +0200

    Just some new icons

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
 var i=Math.floor(Math.random() * 12);
 var weathersList = ['sunrise', 'sunset', 'rain', 'snowfall', 'thunderstorm', 'cloudy', 'cloudy & windy', 'night', 'wind'];
-//var weather = weathersList[i];
-var weather = 'wind';
+var weather = 'sunrise';
+var tomorrowWeather = 'rain';
 var backgroundVideo = document.getElementById('bg_src');
 var icon = document.getElementsByClassName('icon');
 console.log(icon[0])
-if (weather== 'sunrise') {
+if (weather == 'sunrise') {
     backgroundVideo.setAttribute('src', 'video/sunrise.mp4');
-    icon[0].src='img/sunrise.svg';
+    icon[0].src = 'img/sunrise.svg';
 } else if (weather == 'sunset') {
     backgroundVideo.setAttribute('src', 'video/sunset.mp4');
-    icon[0].src='img/sunset.svg';
-}else if (weather == 'rain') {
+    icon[0].src = 'img/sunset.svg';
+} else if (weather == 'rain') {
     backgroundVideo.setAttribute('src', 'video/rain.mp4');
-    //icon[0].src='img/rain.svg';
+    icon[0].src = 'img/raining.svg';
 } else if (weather == 'snowfall') {
     backgroundVideo.setAttribute('src', 'video/snowfall.mp4');
-    icon[0].src='img/snowflake.svg';
+    icon[0].src = 'img/snowflake.svg';
 } else if (weather == 'thunderstorm') {
     backgroundVideo.setAttribute('src', 'video/thunderstorm.mp4');
-    icon[0].src='img/bolt.svg';
+    icon[0].src = 'img/bolt.svg';
 } else if (weather == 'cloudy') {
     backgroundVideo.setAttribute('src', 'video/cloudy.mp4');
-    icon[0].src='img/clouds(1).svg';
+    icon[0].src = 'img/clouds(1).svg';
 } else if (weather == 'cloudyNight') {
     backgroundVideo.setAttribute('src', 'video/cloudyNight.mp4');
 } else if (weather == 'night') {
     backgroundVideo.setAttribute('src', 'video/night.mp4');
-    icon[0].src='img/moon.svg';
+    icon[0].src = 'img/moon.svg';
 } else if (weather == 'wind') {
-    backgroundVideo.setAttribute('src', 'https: //www.pexels.com/video/video-of-wind-turbine-855792/');
-    //icon[0].src = 'img/moon.svg';
+    backgroundVideo.setAttribute('src', 'video/windy & sunny.mp4');
+    icon[0].src = 'img/wind.svg';
+}
+
+if (tomorrowWeather == 'sunrise') {
+    icon[1].src = 'img/sunrise.svg';
+} else if (tomorrowWeather == 'sunset') {
+    icon[1].src = 'img/sunset.svg';
+} else if (tomorrowWeather == 'rain') {
+    icon[1].src = 'img/raining.svg';
+} else if (tomorrowWeather == 'snowfall') {
+    icon[1].src = 'img/snowflake.svg';
+} else if (tomorrowWeather == 'thunderstorm') {
+    icon[1].src = 'img/bolt.svg';
+} else if (tomorrowWeather == 'cloudy') {
+    icon[1].src = 'img/clouds(1).svg';
+} else if (tomorrowWeather == 'wind') {
+    backgroundVideo.setAttribute('src', 'video/windy & sunny.mp4');
+    icon[1].src = 'img/wind.svg';
 }
 
 
