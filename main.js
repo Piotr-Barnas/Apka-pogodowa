988fb4a9f37c8535073bcc957f214b69183b2271
Piotr <piotr.barnas13@gmail.com>
2019-04-08 18:35:09 +0200

    Pasek

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var i=Math.floor(Math.random() * 12);
 var weathersList = ['sunrise', 'sunset', 'rain', 'snowfall', 'thunderstorm', 'cloudy', 'cloudy & windy', 'night', 'wind'];
-var weather = 'sunrise';
+var weather = 'rain';
 var tomorrowWeather = 'rain';
 var cityList = [];
 var backgroundVideo = document.getElementById('bg_src');
@@ -62,20 +62,44 @@ $.ajax({
     }
 });
 
+//var data=['Alabama', 'Atlanta', 'Alcatraz', 'Ateny', 'Atletico', 'Barcelona', 'Boston', 'Bora Bora', 'Bordoux', 'Brussel', 'Bob', 'Boob', 'Londyn']
+
 function filtrowanie() {
-    var input, filter, ul, li, i, txtValue;
-    input = document.getElementById('wyszukiwarka');
-    filter = input.value.toUpperCase();
-    li = ul.getElementsByTagName('li');
-    ul = document.getElementById("pasekMiast");
-    for (i = 0; i < data.length; i++) {
-        txtValue = li[i].innerText;
+    console.log(document.getElementById("pasekMiast").firstElementChild)
+    if (document.getElementById("pasekMiast").childElementCount > 0) {
+        document.getElementById("pasekMiast").removeChild(document.getElementById("pasekMiast").firstElementChild);
+    }
+    var nodeUL = document.createElement("ul");
+    document.getElementById("pasekMiast").appendChild(nodeUL);
+    console.log('zrobione', nodeUL)
+    var input = document.getElementById('wyszukiwarka');
+    var filter = input.value.toUpperCase();
+    for (i = 0; i < cityList.length; i++) {
+        txtValue = cityList[i];
         if (txtValue.toUpperCase().indexOf(filter) == 0 && filter != '') {
-            li[i].style.display = "initial";
-        } else{
-            li[i].style.display = "none";
+            var node = document.createElement("li");                 // Create a <li> node
+            var textnode = document.createTextNode(txtValue);         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <li>
+            document.getElementById("pasekMiast").firstElementChild.appendChild(node);     // Append <li> to <ul> with id="myList"
+        } else {
         }
     }
 }
 
 
+
+/*
+var input, filter, ul, li, i, txtValue;
+input = document.getElementById('wyszukiwarka');
+filter = input.value.toUpperCase();
+li = ul.getElementsByTagName('li');
+ul = document.getElementById("pasekMiast");
+for (i = 0; i < data.length; i++) {
+    txtValue = li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) == 0 && filter != '') {
+        li[i].style.display = "initial";
+    } else {
+        li[i].style.display = "none";
+    }
+}
+*/
